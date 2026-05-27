8fea13a03aaf78d69a233213b764f3d4e2b0997e
unknown <123buruiana456@gmail.com>
2026-05-27 08:21:47 +0300

    Am creat un program

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ro">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farmacia Elody</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <!-- NAVBAR -->
+
+    <nav class="navbar">
+
+        <div class="logo">
+            Elody
+        </div>
+
+        <ul class="menu">
+            <li><a href="#">Acasă</a></li>
+            <li><a href="#">Despre</a></li>
+            <li><a href="#">Produse</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+
+    </nav>
+
+    <!-- HERO SECTION -->
+
+    <section class="hero">
+
+        <div class="hero-text">
+
+            <h1>Farmacia Elody</h1>
+
+            <p>
+                Frumusețe, sănătate și grijă într-un singur loc.
+            </p>
+
+            <button>
+                Descoperă Produsele
+            </button>
+
+        </div>
+
+    </section>
+
+</body>
+</html>
--- text.php
+++ text.php
@@ -1,6 +1,34 @@
+
 <?php
 
 echo "<h1>Farmacia Elody</h1>";
 echo "<p>Sănatatea este cea mai importantă de aceia alegem farmacia Elody cu încredere.</p>";
 
 error_log("Rulez text.php");
+
+
+
+
+$numbers = [3, 17, 12, 7, 5, 20, 14, 9, 2, 11];
+
+$pare = 0;
+$impare = 0;
+
+echo "<h2>Verificare numere pare și impare</h2>";
+
+for ($i = 0; $i < count($numbers); $i++) {
+
+    if ($numbers[$i] % 2 == 0) {
+        echo $numbers[$i] . " este număr PAR <br>";
+        $pare++;
+    } else {
+        echo $numbers[$i] . " este număr IMPAR <br>";
+        $impare++;
+    }
+}
+
+echo "<br>";
+echo "Total numere pare: " . $pare . "<br>";
+echo "Total numere impare: " . $impare;
+?>
+
